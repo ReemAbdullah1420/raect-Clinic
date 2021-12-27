00cfd4642fb233c33bf6f5e0cd4bf0ea6f152e89
ReemAbdullah1420 <Reem.Abdullah.abt.js@tuwaiq.edu.sa>
2021-12-27 13:30:56 +0300

    done

---
+++ clinic/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+      <img src="https://i.pinimg.com/564x/fe/43/9c/fe439c16c456ca4c8b9f725bed501d4b.jpg" style={{ width: "100%" }} />
+
+      <img
+        src="https://banner2.cleanpng.com/20180329/dlq/kisspng-nursing-health-professional-health-care-hospital-nurse-5abcf81f2f6f56.0357020315223337271943.jpg"
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "50%",
+          height: "50vh",
+          objectfit: "cover",
+          zindex: "10",
+        }}
+      />
+    </>
+  )
+}
+
+export default App
